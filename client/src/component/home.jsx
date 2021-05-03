3ce2062dda004c71d26234d6e80ba033baad1e02
Fatin-Shahriare-Sium <sium1206@gmail.com>
2021-05-04 01:31:33 +0600

    done,all the things by the grace of Allah,Alhamdulillah

--- client/src/component/home.jsx
+++ client/src/component/home.jsx
@@ -1,16 +1,33 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import Analytics from './analytics'
+import hero from '../assets/hero.png'
+import './home.css'
 const Home = () => {
     return (
-        <div>
-            <Link to='/signup'>
-            <button>Sign Up</button>
-            </Link>
-            <Link to='/login'>
-            <button>Login</button>
-            </Link>
-            <Analytics/>
+        <div className='home'>
+            <div className="home-nav">
+                <div className="home-nav__title">
+                    <p>Task Manager</p>
+                </div>
+                <div className="home-nav__link">
+                <Link to='/signup'>
+                    <button>Sign Up</button>
+                </Link>
+                <Link to='/login'>
+                    <button>Login</button>
+                </Link>
+                </div>
+            </div>
+            <div className="home-hero container-fluid">
+                <div className="row">
+                    <div className="col-md-6 home-hero__title">
+                        <p>Manage your work smartly<br/> and stay organized with <br/> <span style={{color:'#1bdd70'}}>Task Manager .</span></p>
+                    </div>
+                    <div className="col-md-6 home-hero__img">
+                        <img src={hero} alt=""/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
